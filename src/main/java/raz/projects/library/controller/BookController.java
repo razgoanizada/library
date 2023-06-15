92ec935a74b6bb0b90d8ac1fa018426eb7f8050f
Raz Goanizada <razgoanizada@gmail.com>
2023-06-15 20:41:57 +0300

    test

--- src/main/java/raz/projects/library/controller/BookController.java
+++ src/main/java/raz/projects/library/controller/BookController.java
@@ -13,7 +13,6 @@ import raz.projects.library.dto.update.BookUpdateLocation;
 import raz.projects.library.service.BookService;
 import java.util.List;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
