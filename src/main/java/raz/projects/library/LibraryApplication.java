8405230fbf972e8887eba4dc94ba87c969a16f4d
Raz Goanizada <razgoanizada@gmail.com>
2023-06-15 22:26:50 +0300

    library

--- src/main/java/raz/projects/library/LibraryApplication.java
+++ src/main/java/raz/projects/library/LibraryApplication.java
@@ -7,10 +7,12 @@ public class LibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
+
     }
 
 
     // TODO: 06/06/2023 add error 404 from general errors
     // TODO: 13/06/2023 add role per user
     // TODO: 13/06/2023  disabled security in test
+
 }
