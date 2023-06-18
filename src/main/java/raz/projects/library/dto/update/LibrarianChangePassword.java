84dac46852dd275fecf801f11eade9f05fd74c9a
Raz Goanizada <razgoanizada@gmail.com>
2023-06-18 22:58:38 +0300

    library

---
+++ src/main/java/raz/projects/library/dto/update/LibrarianChangePassword.java
@@ -0,0 +1,23 @@
+package raz.projects.library.dto.update;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import raz.projects.library.validation.password.PasswordMatch;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@PasswordMatch
+public class LibrarianChangePassword {
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?\\W).{8,20}$",
+            message = "The password must be at least 8 characters long, an uppercase letter, a lowercase letter, a number, and a character")
+    private String newPassword;
+    private String repeatNewPassword;
+}
--- src/main/java/raz/projects/library/service/LibrarianServiceImpl.java
+++ src/main/java/raz/projects/library/service/LibrarianServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import raz.projects.library.dto.pages.LibrarianPageDto;
 import raz.projects.library.dto.request.LibrarianRequestDto;
 import raz.projects.library.dto.response.LibrarianResponseDto;
+import raz.projects.library.dto.update.LibrarianChangePassword;
 import raz.projects.library.entity.Librarian;
 import raz.projects.library.enums.Permissions;
 import raz.projects.library.errors.BadRequestException;
@@ -99,12 +100,25 @@ public class LibrarianServiceImpl implements LibrarianService, UserDetailsServic
     }
 
     @Override
-    public LibrarianResponseDto getLibrarianById(long id) {
+    public LibrarianResponseDto getLibrarianById(Long id) {
 
         var librarian = librarianRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException( "get librarian" ,id, "This librarian doesn't exist in the library")
         );
         return mapper.map(librarian, LibrarianResponseDto.class);
+    }
+
+    public LibrarianResponseDto librarianChangePassword(LibrarianChangePassword dto, Long id){
+
+        var librarian = librarianRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException( "get librarian" ,id, "This librarian doesn't exist in the library")
+        );
+
+        librarian.setPassword(passwordEncoder.encode(dto.getNewPassword()));
+
+        var saved = librarianRepository.save(librarian);
+
+        return mapper.map(saved, LibrarianResponseDto.class);
     }
 
     @Override
